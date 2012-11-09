5a454dc409cfe4d029c5d8ddf4d3d393cf84d1d2
Emiliano Burgos <burgosemi@gmail.com>
2012-11-09 15:24:02 -0500

    Added MANIFEST. Included data dir in packaged module. Changed README extension to rst.

--- setup.py
+++ setup.py
@@ -1,30 +1,40 @@
 #! /usr/bin/python
 
 import os
+import glob
 from setuptools import setup, find_packages
 
 from notable import VERSION
 
-description = "A command-line utility to manage Notes."
+description = "A command-line utility to manage notes."
 
 cur_dir = os.path.dirname(__file__)
 try:
-    long_description = open(os.path.join(cur_dir, 'README.md')).read()
+    long_description = open(os.path.join(cur_dir, 'README')).read()
 except:
     long_description = description
 
+datadir = os.path.join(cur_dir, 'data', 'config')
+datafiles = [(datadir, [f for f in glob.glob(os.path.join(datadir, '*'))])]
+print '*' * 20
+print datafiles
+print '*' * 20
 setup(
     name="notable",
     version=VERSION,
-    url='https://github.com/goliatone/notable',
-    license='MIT',
     description=description,
     long_description=long_description,
+    url='https://github.com/goliatone/notable',
+    license='MIT',
     author='goliatone',
     author_email='hello@goliatone.com',
+    keywords='command-line utils notes',
     packages=find_packages('notable',
-                            exclude=['tests', 'experimental']),
+                            exclude=['experimental']),
+                            # exclude=['tests', 'experimental']),
     package_dir={'': 'notable'},
+    # package_data={'': ['config.ini']},
+    data_files=datafiles,
     install_requires=['setuptools', 'cement'],
     entry_points="""
     [console_scripts]
@@ -41,4 +51,5 @@ setup(
         'Topic :: Utilities',
     ],
     # test_suite='nose.collector',
+    test_suite='tests'
 )
